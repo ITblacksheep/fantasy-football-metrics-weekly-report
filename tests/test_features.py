b056fa33e8898ab799ecb39c37e6d914ca340ad5
Rocky Wilson <rockywilson117@gmail.com>
2021-01-02 01:46:40 -0500

    Updated badboy testing

--- tests/test_features.py
+++ tests/test_features.py
@@ -8,7 +8,6 @@ from utils.app_config_parser import AppConfigParser
 module_dir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(module_dir)
 
-from calculate.bad_boy_stats import BadBoyStats
 from calculate.beef_stats import BeefStats
 from calculate.covid_risk import CovidRisk
 
@@ -24,27 +23,6 @@ player_last_name = "Lewis"
 player_full_name = "{0} {1}".format(player_first_name, player_last_name)
 player_team_abbr = "PHI"
 player_position = "RB"
-
-
-def test_bad_boy_init():
-    bad_boy_stats = BadBoyStats(
-        data_dir=test_data_dir,
-        save_data=True,
-        dev_offline=False,
-        refresh=True
-    )
-    bad_boy_stats.generate_crime_categories_json()
-
-    print("Player Bad Boy crime for {0}: {1}".format(
-        player_full_name,
-        bad_boy_stats.get_player_bad_boy_crime(player_full_name, player_team_abbr, player_position)
-    ))
-    print("Player Bad Boy points for {0}: {1}".format(
-        player_full_name,
-        bad_boy_stats.get_player_bad_boy_points(player_full_name, player_team_abbr, player_position)
-    ))
-
-    assert bad_boy_stats.bad_boy_data is not None
 
 
 def test_beef_init():
